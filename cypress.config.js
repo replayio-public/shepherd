e93f5111d426e2a4909035f09443b3c4c63395f5
Jaril <jarilvalenciano@gmail.com>
2022-08-02 20:09:15 -0700

    Add replay to config file

--- cypress.config.js
+++ cypress.config.js
@@ -10,10 +10,13 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      cypressReplay.default(on, config);
       return require('./test/cypress/plugins/index.js')(on, config);
     },
     baseUrl: 'http://localhost:9002',
     specPattern: 'test/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'test/cypress/support/index.js'
+    supportFile: 'test/cypress/support/index.js',
+    screenshotOnRunFailure: false,
+    video: false
   }
 });
