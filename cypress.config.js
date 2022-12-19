b9ce31e561965d1a730ff0f112d135e21d75d641
Jaril <jarilvalenciano@gmail.com>
2022-12-19 15:28:59 -0500

    Track metrics as action-finished-2

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('cypress');
 const cypressReplay = require('@replayio/cypress');
+const { writeFileSync } = require('fs');
 
 module.exports = defineConfig({
   fixturesFolder: 'test/cypress/fixtures',
@@ -9,6 +10,12 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       cypressReplay.default(on, config);
+      on('after:run', (afterRun) => {
+        const data = JSON.stringify(afterRun.totalDuration);
+        const filename = 'duration.json';
+        writeFileSync(filename, data);
+        console.log('cypress-json-results: wrote results to %s', filename);
+      });
       return config;
     },
     baseUrl: 'http://localhost:9002',
